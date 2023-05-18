786cb74ae8268e831d28b273568789e39529cc77
Jongha Park <jongha2788@u.sogang.ac.kr>
2023-05-19 02:43:15 +0900

    feat : kafka Server init

---
+++ dog-kafka-server/src/main/java/com/numble/dogkafkaserver/DogKafkaServerApplication.java
@@ -0,0 +1,13 @@
+package com.numble.dogkafkaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogKafkaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogKafkaServerApplication.class, args);
+    }
+
+}
