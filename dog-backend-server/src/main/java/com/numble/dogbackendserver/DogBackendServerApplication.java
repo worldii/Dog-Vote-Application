48bde95cf744b114950457f565315f0575935870
Jongha Park <jongha2788@u.sogang.ac.kr>
2023-05-19 02:38:56 +0900

    Feat : init backEndServer

---
+++ dog-backend-server/src/main/java/com/numble/dogbackendserver/DogBackendServerApplication.java
@@ -0,0 +1,13 @@
+package com.numble.dogbackendserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogBackendServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogBackendServerApplication.class, args);
+    }
+
+}
